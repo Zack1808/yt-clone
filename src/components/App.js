d3ab457ff4bae95e26abeeb21894f10cdf2b26fd
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-02-28 21:25:40 +0100

    Created new App component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Creating the App component
+const App = () => {
+  return <div className="app-component"></div>;
+};
+
+// Exporting the App component
+export default App;
