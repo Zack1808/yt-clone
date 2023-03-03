8639aac416fdf6759e470774129a086a24398406
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-03-03 07:28:40 +0100

    Added the SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  UilEstate,
+  UilMusic,
+  UilBrain,
+  UilArrow,
+} from "@iconscout/react-unicons";
+
+// Creating the SideBar component
+const SideBar = () => {
+  return <div className="sidebar-container"></div>;
+};
+
+// Exporting the SideBar component
+export default SideBar;
